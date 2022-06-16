c57b98c7ae5b418ee408a5e04a233f649107f72d
Matthew Carby <matthewcarby18@gmail.com>
2022-06-15 22:08:30 -0400

    converted tag to functional component

--- knowledgeBase/frontend/src/components/Tag/Tag.js
+++ knowledgeBase/frontend/src/components/Tag/Tag.js
@@ -1,44 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Tag.css";
 import $ from 'jquery';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
-class Tag extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tagName: this.props.tagDetails.name,
-      tagColor: this.props.tagDetails.color,
-      // clicks: 0, //keep track of how many times user clicks tag for delete functionality
-    }
 
-    this.getCookie = this.getCookie.bind(this);
-    this.setTagColor = this.setTagColor.bind(this);
-    this.updateTag = this.updateTag.bind(this);
-    this.onTagClick = this.onTagClick.bind(this);
-    this.onTagHover = this.onTagHover.bind(this);
-    this.onTagExitHover = this.onTagExitHover.bind(this);
-  }
+const Tag = (props) => {
+  const [tagName, setTagName] = useState(props.tagDetails.name)
+  const [tagColor, setTagColor] = useState(props.tagDetails.color)
 
-  componentDidMount() {
-    this.setTagColor();
-  }
+  useEffect(() => {
+    fillTagColor();
+  }, [tagColor])
 
-  componentDidUpdate() {
-    this.setTagColor();
-    console.log(this.props.deleting);
-    if (!this.props.deleting) {
-      const delTags = document.getElementsByClassName("del-clicked");
-      if (delTags.length > 0) {
-        delTags[0].classList.remove("del-clicked");
-      }
-    }
-  }
-
-
-  getCookie(name) {
+  const getCookie = (name) => {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
       const cookies = document.cookie.split(';');
@@ -54,15 +30,23 @@ class Tag extends React.Component {
     return cookieValue;
   }
 
+  useEffect(() => {
+    if (!props.deleting) {
+      const delTags = document.getElementsByClassName("del-clicked");
+      if (delTags.length > 0) {
+        delTags[0].classList.remove("del-clicked");
+      }
+    }
+  }, [props.deleting])
 
-  setTagColor() {
-    const tag = document.getElementById(`tag-element-color${this.props.tagKey}`);
-    tag.style.backgroundColor = this.state.tagColor;
+  const fillTagColor = () => {
+    const tag = document.getElementById(`tag-element-color${props.tagKey}`);
+    tag.style.backgroundColor = tagColor;
   }
 
-  deleteTag() {
-    const url = `http://127.0.0.1:8000/api/deleteTag/${this.props.tagDetails.id}/`;
-    const csrftoken = this.getCookie('csrftoken');
+  const deleteTag = () => {
+    const url = `http://127.0.0.1:8000/api/deleteTag/${props.tagDetails.id}/`;
+    const csrftoken = getCookie('csrftoken');
 
     fetch(url, {
       method: "DELETE",
@@ -70,26 +54,22 @@ class Tag extends React.Component {
         "Content-type": "application/json",
         "X-CSRFToken": csrftoken,
       },
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState(state => {
-          return { clicks: 0 }
-        })
-      })
+    })
+
   }
 
-  onTagClick() {
-    if (!this.props.deleting) {
-      const tag = document.getElementById(`tag-element-color${this.props.tagKey}`)
+
+  const onTagClick = () => {
+    if (!props.deleting) {
+      const tag = document.getElementById(`tag-element-color${props.tagKey}`)
       tag.style.transform = "scale(1.15)";
-      $(`#update-tag-form${this.props.tagKey}`).slideDown("fast");
+      $(`#update-tag-form${props.tagKey}`).slideDown("fast");
     } else {
-      const tagElem = document.getElementById(`tag-element-color${this.props.tagKey}`);
-      const clickedElems = document.getElementsByClassName("del-clicked")//prev clicked elems 
+      const tagElem = document.getElementById(`tag-element-color${props.tagKey}`);
+      const clickedElems = document.getElementsByClassName("del-clicked");//prev clicked elems 
       if (clickedElems.length > 0) {
         if (tagElem == clickedElems[0]) {
-          this.deleteTag();
+          deleteTag();
           window.location.reload();
         } else {
           clickedElems[0].classList.remove("del-clicked");
@@ -99,32 +79,30 @@ class Tag extends React.Component {
         tagElem.classList.add("del-clicked");
       }
     }
+  }
 
+  const onTagHover = () => {
+    const updateForm = document.getElementById(`update-tag-form${props.tagKey}`);
+    $(`#update-tag-form${props.tagKey}`).slideToggle("fast");
 
   }
 
-  onTagHover() {
-    const updateForm = document.getElementById(`update-tag-form${this.props.tagKey}`);
-    $(`#update-tag-form${this.props.tagKey}`).slideToggle("fast");
-
-
-  }
-
-  onTagExitHover() {
-    const updateForm = document.getElementById(`update-tag-form${this.props.tagKey}`);
+  const onTagExitHover = () => {
+    const updateForm = document.getElementById(`update-tag-form${props.tagKey}`);
     updateForm.reset();
-    const tag = document.getElementById(`tag-element-color${this.props.tagKey}`)
+    const tag = document.getElementById(`tag-element-color${props.tagKey}`)
     tag.style.transform = "scale(1)";
-    $(`#update-tag-form${this.props.tagKey}`).slideUp("fast");
+    $(`#update-tag-form${props.tagKey}`).slideUp("fast");
   }
 
-  updateTag(e) {
+  const updateTag = (e) => {
     e.preventDefault();
-    const url = `http://127.0.0.1:8000/api/updateTag/${this.props.tagDetails.id}/`;
-    const csrftoken = this.getCookie('csrftoken');
 
-    const tName = document.getElementById(`tag-grid-name${this.props.tagKey}`).value;
-    const tColor = document.getElementById(`tag-grid-color${this.props.tagKey}`).value;
+    const url = `http://127.0.0.1:8000/api/updateTag/${props.tagDetails.id}/`;
+    const csrftoken = getCookie('csrftoken');
+
+    const tName = document.getElementById(`tag-grid-name${props.tagKey}`).value;
+    const tColor = document.getElementById(`tag-grid-color${props.tagKey}`).value;
     fetch(url, {
       method: "POST",
       headers: {
@@ -134,32 +112,29 @@ class Tag extends React.Component {
       body: JSON.stringify({ name: tName, color: tColor })
     }).then(response => response.json())
       .then(data => {
-        this.setState(state => {
-          return { tagName: tName, tagColor: tColor }
-        })
+        setTagName(data.name);
+        setTagColor(data.color);
       })
   }
 
-  render() {
-    return (
-      <div id={`tag-element-color${this.props.tagKey}`} className="tag-grid-element" onClick={this.onTagClick} onMouseLeave={this.onTagExitHover}>
-        <div className="tag-element-holder">
-          <p className="tag-element-name">{this.state.tagName}</p>
-          <div className="tag-grid-form-holder">
-            <form id={`update-tag-form${this.props.tagKey}`} className="tag-grid-form">
-              <div className="form-group">
-                <input id={`tag-grid-name${this.props.tagKey}`} className="form-control tag-grid-name-field" required autoComplete="off" defaultValue={this.state.tagName} />
-                <div className="color-save-btn-holder">
-                  <input id={`tag-grid-color${this.props.tagKey}`} className="form-control tag-grid-color-field" type="color" defaultValue={this.state.tagColor} />
-                  <div className="tick-icon-holder"><FontAwesomeIcon icon={solid("circle-check")} onClick={this.updateTag} /></div>
-                </div>
+  return (
+    <div id={`tag-element-color${props.tagKey}`} className="tag-grid-element" onClick={onTagClick} onMouseLeave={onTagExitHover}>
+      <div className="tag-element-holder">
+        <p className="tag-element-name">{tagName}</p>
+        <div className="tag-grid-form-holder">
+          <form id={`update-tag-form${props.tagKey}`} className="tag-grid-form">
+            <div className="form-group">
+              <input id={`tag-grid-name${props.tagKey}`} className="form-control tag-grid-name-field" required autoComplete="off" defaultValue={tagName} />
+              <div className="color-save-btn-holder">
+                <input id={`tag-grid-color${props.tagKey}`} className="form-control tag-grid-color-field" type="color" defaultValue={tagColor} />
+                <div className="tick-icon-holder"><FontAwesomeIcon icon={solid("circle-check")} onClick={updateTag} /></div>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Tag;
